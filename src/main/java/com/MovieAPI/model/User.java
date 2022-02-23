35a7ed2eebb9432188449ea530e044a52851d7cc
Gurpreet1490 <g.preet08092014@gmail.com>
2022-02-23 08:25:54 +0000

    few changes

--- src/main/java/com/MovieAPI/model/User.java
+++ src/main/java/com/MovieAPI/model/User.java
@@ -1,5 +1,7 @@
 package com.MovieAPI.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
