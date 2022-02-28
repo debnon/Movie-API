f12c9058b21053a569016c69f96f2f66d518fe8d
panthangiswapna <pspsowmya@gmail.com>
2022-02-28 23:34:45 +0000

    config: added Springboot actuator info and health endpoints and added swagger ui

--- src/main/java/com/MovieAPI/MovieApiApplication.java
+++ src/main/java/com/MovieAPI/MovieApiApplication.java
@@ -7,17 +7,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 //import org.springframework.asm.TypeReference;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class MovieApiApplication {
 
 	public static void main(String[] args) {
@@ -46,5 +56,24 @@ public class MovieApiApplication {
 //	public WebClient.Builder getWebClientBuilder() {
 //		return WebClient.builder();
 //	}
+
+
+	@Bean
+	public Docket swaggerConfiguration(){
+		return new Docket(DocumentationType.OAS_30)
+				.select()
+				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+				.build()
+				.apiInfo(apiInformation());
+	}
+
+	private ApiInfo apiInformation(){
+		return new ApiInfoBuilder()
+				.title("Movies API")
+				.description("Need to know details about movies..? Here is the API for you...!!! ")
+				.version("v1")
+				.build();
+	}
 
 }
