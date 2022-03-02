b425a980cbdf312dde00f962f2ccfb9d1be71ef2
Ben Donaghy <bendonaghy321@gmail.com>
2022-03-02 22:36:37 +0000

    added routing for 4 basic pages, basic layout, and moved userlist functionality to its own component

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,43 +1,36 @@
 import React, {useState, useEffect} from "react";
+import ReactDOM from "react-dom";
 import axios from "axios";
 
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
+import Preferences from './components/preferences/Preferences';
+import Userlist from './components/userlist/Userlist';
+import Layout from './components/Layout';
+import Home from './components/Home';
+
 import './App.css';
 
-const UserProfiles = () => {
 
-    const [userProfiles, setUserProfiles] = useState([]);
 
-    const fetchUserProfiles =  () => {
-        axios.get("http://localhost:8080/api/v1/admin").then(res => {
-            console.log(res);
-            // const data = res.data;
-            setUserProfiles(res.data);
-        });
-    };
-
-// similar to componentDidMount/componentDidUpdate, [] calls this effect when the user array changes 
-    useEffect(() => {
-        fetchUserProfiles();
-    }, []);
-
-    return userProfiles.map((userProfile, index) => {
-        return (
-        <div key={index}>
-            <h1>{userProfile.username}</h1>
-            <p>{userProfile.id}</p>
-        </div>
-        )
-    })
-};
-
-function App() {
+export default function App() {
   return (
-      <div className="App">
-          <UserProfiles />
-      </div>
+    
+    
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="dashboard" element={<Dashboard />}/>
+                <Route path="preferences" element={<Preferences />}/>
+                <Route path="userlist" element={<Userlist />}/>
+            </Route>
+        </Routes>
+      </BrowserRouter>
+      
     
   );
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById("root"));
+// export default App;
