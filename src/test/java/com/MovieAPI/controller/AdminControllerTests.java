85bca508ee727c8f400fc87094192a16f9b1ba46
panthangiswapna <pspsowmya@gmail.com>
2022-03-05 20:11:42 +0000

    test: implemented test methods for movie endpoints in admin controller

--- src/test/java/com/MovieAPI/controller/AdminControllerTests.java
+++ src/test/java/com/MovieAPI/controller/AdminControllerTests.java
@@ -1,7 +1,10 @@
 package com.MovieAPI.controller;
 
+import com.MovieAPI.model.Genre;
+import com.MovieAPI.model.Movie;
 import com.MovieAPI.model.Roles;
 import com.MovieAPI.model.User;
+import com.MovieAPI.service.MovieServiceImpl;
 import com.MovieAPI.service.UserServiceImpl;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -32,6 +35,9 @@ public class AdminControllerTests {
     @Mock
     private UserServiceImpl mockUserServiceImpl;
 
+    @Mock
+    private MovieServiceImpl mockMovieServiceImpl;
+
     @Autowired
     private MockMvc mockMvcController;
 
@@ -43,6 +49,7 @@ public class AdminControllerTests {
         mapper = new ObjectMapper();
     }
 
+    //Test methods for all the user end points
     @Test
     public void testGetMappingGetAllUsers() throws Exception {
         //Arrange
@@ -139,5 +146,120 @@ public class AdminControllerTests {
 
     }
 
+    //Test methods for all the movie end points
+    @Test
+    public void testGetMappingGetAllMovies() throws Exception {
+        //Arrange
+        List<Genre> genreList = new ArrayList();
+        genreList.add(Genre.Adventure);
+        genreList.add(Genre.Fantasy);
+
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie(1L, "Movie1", "This is description of Movie1", "22-02-22", "A", "English", genreList, "poster1", "bd1", 0200L, "NA", "100"));
+        movies.add(new Movie(2L, "Movie2", "This is description of Movie2", "19-08-93", "A", "English", genreList, "poster2", "bd2", 0300L, "NA", "200"));
+        movies.add(new Movie(3L, "Movie3", "This is description of Movie3", "01-11-18", "A", "English", genreList, "poster3", "bd3", 0145L, "NA", "300"));
+
+        //Act and Assert
+        when(mockMovieServiceImpl.getAllMovies()).thenReturn(movies);
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/api/v1/admin/movie/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].title").value("Movie1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].title").value("Movie2"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].id").value(3))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].title").value("Movie3"));
+    }
+
+    @Test
+    public void testPostMappingAddMovie() throws Exception {
+        //Arrange
+        List<Genre> genreList = new ArrayList();
+        genreList.add(Genre.Drama);
+        genreList.add(Genre.Crime);
+
+        Movie newMovie = new Movie(4L, "Movie4", "This is description of Movie4", "12-07-86", "A", "English", genreList, "poster4", "bd4", 0145L, "NA", "400");
+
+        //Act
+        when(mockMovieServiceImpl.insertMovie(newMovie)).thenReturn(newMovie);
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.post("/api/v1/admin/movie/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(newMovie)))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+
+        //Assert
+        verify(mockMovieServiceImpl, times(1)).insertMovie(newMovie);
+
+    }
+
+    @Test
+    public void testGetMappingGetMovieById() throws Exception {
+        //Arrange
+        List<Genre> genreList = new ArrayList();
+        genreList.add(Genre.Drama);
+        genreList.add(Genre.Crime);
+
+        Movie movie = new Movie(4L, "Movie4", "This is description of Movie4", "12-07-86", "A", "English", genreList, "poster4", "bd4", 0145L, "NA", "400");
+
+        //Act and Assert
+        when(mockMovieServiceImpl.getMovieById(movie.getId())).thenReturn(movie);
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/api/v1/admin/movie/" + movie.getId()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(4))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Movie4"));
+    }
+
+    @Test
+    public void testPutMappingUpdateAMovie() throws Exception {
+        //Arrange
+        List<Genre> genreList = new ArrayList();
+        genreList.add(Genre.Drama);
+        genreList.add(Genre.Crime);
+
+        Movie movie = new Movie(4L, "Movie4", "This is description of Movie4", "12-07-86", "A", "English", genreList, "poster4", "bd4", 0145L, "NA", "400");
+
+
+        //Act
+        when(mockMovieServiceImpl.getMovieById(movie.getId())).thenReturn(movie);
+        movie.setTitle("Movie Four");
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.put("/api/v1/admin/movie/" + movie.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(movie)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        //Assert
+        verify(mockMovieServiceImpl, times(1)).updateMovieById(movie.getId(), movie);
+    }
+
+    @Test
+    public void testDeleteMappingDeleteAMovie() throws Exception {
+        //Arrange
+        List<Genre> genreList = new ArrayList();
+        genreList.add(Genre.Drama);
+        genreList.add(Genre.Crime);
+
+        Movie movie = new Movie(4L, "Movie4", "This is description of Movie4", "12-07-86", "A", "English", genreList, "poster4", "bd4", 0145L, "NA", "400");
+
+
+        //Act
+        when(mockMovieServiceImpl.getMovieById(movie.getId())).thenReturn(movie);
+
+        this.mockMvcController.perform(MockMvcRequestBuilders.delete("/api/v1/admin/movie/" + movie.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(movie)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        //Assert
+        verify(mockMovieServiceImpl, times(1)).deleteMovieById(movie.getId());
+
+    }
 
 }
