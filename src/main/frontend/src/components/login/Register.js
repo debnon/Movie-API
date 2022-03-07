48055d4f01ab916438aa8ff20c21aefb9352d479
Ben Donaghy <bendonaghy321@gmail.com>
2022-03-07 03:06:34 +0000

    registration animation and background refactored

--- src/main/frontend/src/components/login/Register.js
+++ src/main/frontend/src/components/login/Register.js
@@ -4,30 +4,12 @@ import './Register.css';
 const Register = ({ isShowRegister }) => {
 
   return (
-    // <div className={`${isShowRegister ? "active" : ""} show`}>
-    //   <div className="register-form">
-    //     <div className="form-box solid">
-    //       <form>
-    //         <h1 className="register-text">Sign In</h1>
-    //         <label>Username</label>
-    //         <br></br>
-    //         <input type="text" name="username" className="register-box" />
-    //         <br></br>
-    //         <label>Password</label>
-    //         <br></br>
-    //         <input type="password" name="password" className="register-box" />
-    //         <br></br>
-    //         <input type="submit" value="register" className="register-btn" />
-    //       </form>
-    //     </div>
-    //   </div>
-    // </div>
     <div className={`${isShowRegister ? "active" : ""} show`}>
         
     <div class="form_wrapper">
   <div class="form_container">
     <div class="title_container">
-      <h2>Responsive Registration Form</h2>
+      <h2>Sign up to movieroll now</h2>
     </div>
     <div class="row clearfix">
       <div class="">
