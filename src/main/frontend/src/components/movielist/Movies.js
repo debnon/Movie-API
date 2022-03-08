510bd0aadb26a57d5fc907dba9f54042704acfcf
Ben Donaghy <bendonaghy321@gmail.com>
2022-03-08 11:09:43 +0000

    added basic search bar to movielist (intended homepage)

--- src/main/frontend/src/components/movielist/Movies.js
+++ src/main/frontend/src/components/movielist/Movies.js
@@ -33,43 +33,28 @@ const MovieProfiles = () => {
     return movieProfiles.map((movieProfile, index) => {
         var weburl = "https://image.tmdb.org/t/p/original/".concat(movieProfile.poster);
         return (
-        <div key={index}>
-            <pre><h1>{movieProfile.id}: {movieProfile.description}, {movieProfile.releaseDate}</h1></pre>
-            
-            {/* <img src={weburl}
-alt="Movie!" className="imghandle"/> */}
-<div class="hero-container">
-		<div class="main-container">
-			<div class="poster-container">
-				<a href="#"><img src={weburl} class="poster" /></a>
-			</div>
-			<div class="ticket-container">
-				<div class="ticket__content">
-					<h4 class="ticket__movie-title">{movieProfile.title}</h4>
-					<p class="ticket__movie-slogan">
-						More human than human is our motto.
-					</p>
-					{/* <p class="ticket__current-price">$28.00</p>
-					<p class="ticket__old-price">$44.99</p>
-					<button class="ticket__buy-btn">Buy now</button> */}
-				</div>
-			</div>
-		</div>
-
-		
-	</div>
-            
-            <p>Rating: {movieProfile.rating}</p>
-        </div>
+            <div class="card"><img src={weburl} class="poster" /></div>
         )
     })
 };
 
 export default function Movielist() {
     return(
-        <div className="movielist">
-        <h1>Application</h1>
-                <MovieProfiles />
-            </div>
+        <>
+        <form class="example">
+  <input type="text" placeholder="Search.." name="search"/>
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form>
+        <section>
+  <h2>All movies</h2>
+  <div class="cards-wrapper">
+  <MovieProfiles />
+  </div>
+</section>
+         {/* <div className="movielist">
+         <h1>Application</h1>
+                 <MovieProfiles />
+             </div> */}
+             </>
     );
   }
